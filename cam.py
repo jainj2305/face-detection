2e1da6d4d0a9709cb8a3902354c598e5245b88bc
Jinendra Jain <jainj2305@gmail.com>
2020-08-12 22:59:28 +0530

    Removed use of matplotlib.

--- cam.py
+++ cam.py
@@ -1,28 +1,20 @@
 import cv2
-import matplotlib.pyplot as plt
+'''
+camera = cv2.VideoCapture(0)
 
-camera = cv2.VideoCapture(0)  #0 is passed for primary camera you can change it with external camera by passing 1 2 ..
-
-success,pic =camera.read()  #starts camera of your system
+success,pic =camera.read()
 
 camera.release()
-
-if(success):
-	pic = cv2.cvtColor(pic,4)
-	plt.imshow(pic)
-	plt.show()
-	print(pic.shape)
-	
-
-face_model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  #detect faces
+'''
+pic = cv2.imread('jj.jpg')
+face_model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 faces = face_model.detectMultiScale(pic)
 
-print(faces)
 
 for face in faces:
         x,y,w,h=face
         print(x,y,w,h)
-        pic = cv2.rectangle(pic,(x,y),(x+w,y+h),(0,255,0),3)
-plt.imshow(pic)
-plt.axis("off")
-plt.show()
+        pic = cv2.rectangle(pic,(x,y),(x+w,y+h),(0,0,255),1)
+cv2.imshow("face detection",pic)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
